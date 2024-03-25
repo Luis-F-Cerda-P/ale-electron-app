0ae70866f1a0894206c753593a7d0fd060d1d4ad
Luis-F-Cerda-P <luis.f.cerda.p@gmail.com>
2024-03-25 09:31:35 -0300

    Added some comments to the file-system-feature files, refactored some of the template-generating function and correctly implemented the easy-template-x module. Will now proceed to fullfill the template modification requirements and modify the form, since its implementation was made according to a specification file, but there was more information inside the template that has to be taken into account

--- file-system-features/select-target-folder.js
+++ file-system-features/select-target-folder.js
@@ -18,9 +18,9 @@ const selectFolder = async () => {
     if (!result.canceled) {
       const folderPath = result.filePaths[0];
       console.log('Selected folder:', folderPath);
-      await setDefaultFolderSetting(folderPath); 
+      await setDefaultFolderSetting(folderPath);
       return folderPath;
-    } 
+    }
   } catch (err) {
     console.error(err);
   }
@@ -29,7 +29,7 @@ const selectFolder = async () => {
 const createProposal = async (_event, formData) => {
   try {
     // TODO: Make the correlative ID dynamic and configurable. Even pass it to the front end so it's part of the form. On click, it can be modified through a form inside a modal
-    const basePath = await getDefaultFolderSetting(); 
+    const basePath = await getDefaultFolderSetting();
     const year = new Date().getFullYear() - 2000;
     const correlative = "025"
     // TODO: validate the formObject in the frontend, then here. Maybe do some of this process in the front process.
@@ -37,23 +37,31 @@ const createProposal = async (_event, formData) => {
     let proposalName = `\\P${year}${correlative} - AS - ${formObject.cliente}`
 
     if (formObject.ano_licitacion) formObject.ano_licitacion = formObject.ano_licitacion.slice(2, 4)
-    if (formObject["ente-publico"]) proposalName += ` - ${formObject.tipo_venta} ${formObject.codigo_licitacion}.${formObject.ano_licitacion}` 
+    if (formObject["ente-publico"]) proposalName += ` - ${formObject.tipo_venta} ${formObject.codigo_licitacion}.${formObject.ano_licitacion}`
     if (formObject.descripcion) proposalName += ` - ${formObject.descripcion}`
-    
+
     // TODO: Get file templates dynamically. Find the files from a database reference => 'Tomate creation' UI
     const folderPath = path.join(basePath, proposalName,);
     const excelTemplateSource = `.\\plantillaExcel.xlsx`
     const excelFileDestination = folderPath + proposalName + ".xlsx";
-    const wordTemplateSource = `.\\plantillaWord.docx`
+    const wordTemplateSource = `.\\plantillaWord 1.docx`
     const wordFileDestination = folderPath + proposalName + ".docx";
 
     fs.mkdirSync(folderPath);
     fs.copyFileSync(excelTemplateSource, excelFileDestination)
     fs.copyFileSync(wordTemplateSource, wordFileDestination)
+
+    const newWordFile = fs.readFileSync(wordFileDestination); 
+    const replacementData = {
+      "cliente": formObject.cliente
+    }
+    const handler = new TemplateHandler(); 
+    const doc = await handler.process(newWordFile, replacementData)
+    fs.writeFileSync(wordFileDestination, doc)
     shell.openPath(folderPath);
 
     return folderPath;
-  } catch(err) {
+  } catch (err) {
     console.log(err)
   }
 }
