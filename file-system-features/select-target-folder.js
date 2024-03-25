7d46c8eb57e7b14e8ac137d2a88aa6b3135de1b3
Luis-F-Cerda-P <luis.f.cerda.p@gmail.com>
2024-03-25 07:37:12 -0300

    Refactored the file-copying and folder-creation process of the quasi-hard-coded (for now) automation. Added easy-template-x as a module. Did some minor changes to the CSS

--- file-system-features/select-target-folder.js
+++ file-system-features/select-target-folder.js
@@ -1,6 +1,9 @@
 const { dialog } = require('electron')
+const { shell } = require('electron');
+const path = require('node:path');
 const fs = require("node:fs")
 const { db, getDefaultFolderSetting, setDefaultFolderSetting } = require('../database/main')
+const { TemplateHandler } = require('easy-template-x')
 
 
 const selectFolder = async () => {
@@ -23,26 +26,32 @@ const selectFolder = async () => {
   }
 };
 
-const createProposal = async (event, formData) => {
+const createProposal = async (_event, formData) => {
   try {
-    const formObject = Object.fromEntries(formData.filter(el => el[1]).map(el => [el[0], el[1].trim()]))
-    if (formObject.ano_licitacion) formObject.ano_licitacion = formObject.ano_licitacion.slice(2, 4)
+    // TODO: Make the correlative ID dynamic and configurable. Even pass it to the front end so it's part of the form. On click, it can be modified through a form inside a modal
     const basePath = await getDefaultFolderSetting(); 
     const year = new Date().getFullYear() - 2000;
     const correlative = "025"
+    // TODO: validate the formObject in the frontend, then here. Maybe do some of this process in the front process.
+    const formObject = Object.fromEntries(formData.filter(el => el[1]).map(el => [el[0], el[1].trim()]))
     let proposalName = `\\P${year}${correlative} - AS - ${formObject.cliente}`
+
+    if (formObject.ano_licitacion) formObject.ano_licitacion = formObject.ano_licitacion.slice(2, 4)
     if (formObject["ente-publico"]) proposalName += ` - ${formObject.tipo_venta} ${formObject.codigo_licitacion}.${formObject.ano_licitacion}` 
     if (formObject.descripcion) proposalName += ` - ${formObject.descripcion}`
-    // proposalName += "\""
-    const path = require('path');
-    const folderPath = path.join(basePath, proposalName, "\\.");
-    console.log(folderPath);
-    fs.mkdirSync(folderPath);
+    
+    // TODO: Get file templates dynamically. Find the files from a database reference => 'Tomate creation' UI
+    const folderPath = path.join(basePath, proposalName,);
+    const excelTemplateSource = `.\\plantillaExcel.xlsx`
+    const excelFileDestination = folderPath + proposalName + ".xlsx";
+    const wordTemplateSource = `.\\plantillaWord.docx`
+    const wordFileDestination = folderPath + proposalName + ".docx";
 
-    const { shell } = require('electron');
+    fs.mkdirSync(folderPath);
+    fs.copyFileSync(excelTemplateSource, excelFileDestination)
+    fs.copyFileSync(wordTemplateSource, wordFileDestination)
     shell.openPath(folderPath);
-    fs.copyFileSync("C:\\Users\\luisc\\Documents\\Programacion\\Alejandra\\ale-electron-app\\plantillaExcel.xlsx", folderPath + proposalName + ".xlsx")
-    fs.copyFileSync("C:\\Users\\luisc\\Documents\\Programacion\\Alejandra\\ale-electron-app\\plantillaWord.docx", folderPath + proposalName + ".docx")
+
     return folderPath;
   } catch(err) {
     console.log(err)
